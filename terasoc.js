46ef5bd396f24e4509b6afd2859dff8684b4de44
PancenOye007 <115642214+PancenOye007@users.noreply.github.com>
2024-11-10 20:23:49 +0700

    Update terasoc.js

--- terasoc.js
+++ terasoc.js
@@ -1,1 +1,1 @@
-type='text/javascript' src='//pl16052046.highcpmrevenuenetwork.com/10/da/2e/10da2e87caf952bed030dcb23768c37b.js'
+type='text/javascript' src='//pl16052046.profitablecpmrate.com/10/da/2e/10da2e87caf952bed030dcb23768c37b.js'
